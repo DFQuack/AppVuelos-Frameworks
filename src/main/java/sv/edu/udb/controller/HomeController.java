f50920732ffcfbd19a0680b3b2ac35f14cb5a805
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2025-10-23 22:58:44 -0600

    algunos mvc faltan

--- src/main/java/sv/edu/udb/controller/HomeController.java
+++ src/main/java/sv/edu/udb/controller/HomeController.java
@@ -7,11 +7,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/index.html";
+    }
 
     @GetMapping("/vuelos")
     public String redirectToVuelos() {
         return "redirect:/Vuelo.html";
     }
-
+    @GetMapping("/Reservaciones")
+    public String redirectToReservaciones() {
+        return "redirect:/ReservacionForm.html";
+    }
+    @GetMapping("/Pasajero")
+    public String redirectToPasajero() {
+        return "redirect:/PasajeroForm.html";
+    }
 
 }
