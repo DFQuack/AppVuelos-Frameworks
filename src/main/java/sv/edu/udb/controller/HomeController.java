056816ef1e639163771eef902dd1697e8ed3f153
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2025-10-23 21:35:12 -0600

    Vistas modificas Vuelos MVC implementado

--- src/main/java/sv/edu/udb/controller/HomeController.java
+++ src/main/java/sv/edu/udb/controller/HomeController.java
@@ -1,7 +1,17 @@
+
 package sv.edu.udb.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
+
+
+    @GetMapping("/vuelos")
+    public String redirectToVuelos() {
+        return "redirect:/Vuelo.html";
+    }
+
+
 }
